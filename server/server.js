2d159b48537fac556a462519e7feac1a37c47831
bxgio <114640185+bxgio@users.noreply.github.com>
2023-12-11 22:20:26 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MySQL Connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mern_login_db',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('MySQL connection error:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
+
+// Routes
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+
+  db.query(query, [username, password], (err, results) => {
+    if (err) {
+      console.error('MySQL query error:', err);
+      res.json({ success: false, message: 'Error during login' });
+    } else if (results.length > 0) {
+      res.json({ success: true, message: 'Login successful' });
+    } else {
+      res.json({ success: false, message: 'Invalid credentials' });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
